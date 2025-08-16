2b234c4aff02a8e6ce7690e7d1ce9af15fba5064
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2025-08-17 00:12:59 +0900

    feat: add CountDownTimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { CountdownTimer } from "@/components/CountdownTimer";
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen items-center justify-items-center">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">TSKaigi Hokuriku</h1>
+      <CountdownTimer />
     </div>
   );
 }
---
+++ src/components/CountdownTimer/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useCountdown } from "./hooks";
+
+export function CountdownTimer() {
+  const { days, hours, minutes, seconds } = useCountdown();
+
+  return (
+    <div className="rounded-xl bg-blue-600 shadow-lg backdrop-blur-sm">
+      <div className="m-10 flex items-center justify-center gap-3 space-x-4 text-white">
+        <TimeLeft value={days} unit="Days" />
+        <Divider />
+        <TimeLeft value={hours} unit="Hours" />
+        <Divider />
+        <TimeLeft value={minutes} unit="Minutes" />
+        <Divider />
+        <TimeLeft value={seconds} unit="Seconds" />
+      </div>
+    </div>
+  );
+}
+
+function TimeLeft({ value, unit }: { value: number; unit: string }) {
+  return (
+    <div className="text-center">
+      <span className="block text-4xl font-bold">{value}</span>
+      <span className="text-xs">{unit}</span>
+    </div>
+  );
+}
+
+function Divider() {
+  return <span className="-mt-4 h-6 w-[1px] rotate-[15deg] bg-current" aria-hidden="true" />;
+}
