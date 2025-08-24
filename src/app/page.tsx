f468c0506e6aae2e3dfe6eeea933c112f14ccf0b
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2025-08-25 00:43:57 +0900

    feat: add hero section with grid mask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
 import { CountdownTimer } from "@/components/countdown-timer";
+import { ChevronDown } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl font-bold">TSKaigi Hokuriku</h1>
-      <CountdownTimer />
-    </div>
+    <main className="overflow-x-hidden pt-8">
+      <div className="relative flex h-[674px] w-full flex-col items-center text-center lg:h-[790px]">
+        {/* grid overlay */}
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 -z-10 bg-[length:42px_42px] opacity-15 lg:bg-[length:55px_55px]"
+          style={{
+            backgroundImage: `
+            linear-gradient(var(--primary) 2px, transparent 2px),
+            linear-gradient(90deg, var(--primary) 2px, transparent 2px)
+          `,
+            mask: `radial-gradient(
+            ellipse 70% 60% at center,
+            black 20%,
+            black 20%,
+            transparent 60%
+          )`,
+            WebkitMask: `radial-gradient(
+            ellipse 70% 60% at center,
+            black 20%,
+            black 20%,
+            transparent 60%
+          )`,
+          }}
+        />
+        <div className="mt-[178px] lg:mt-[248px]">
+          <CountdownTimer />
+        </div>
+        <div className="mt-[52px] space-y-1 text-2xl font-semibold">
+          <time
+            dateTime="2025-11-03"
+            className="text-[28px] leading-[36px] font-semibold tracking-[-.75%] tabular-nums"
+          >
+            11/3
+          </time>
+          <p className="font-noto text-[23px] leading-[36px] font-bold tracking-[-.75%]">
+            ホテル金沢
+          </p>
+        </div>
+        <h1 className="text-primary mt-[30px] w-96 text-[50px] leading-[50px] font-bold tracking-[0.2%] whitespace-nowrap lg:mt-[40px] lg:w-full lg:text-[59px]">
+          TSKaigi Hokuriku
+          <wbr /> Coming Soon
+        </h1>
+        <div className="mt-11 flex flex-col items-center lg:mt-16">
+          <div className="text-primary text-xl leading-[25px] font-semibold tracking-[-1.2%] lg:font-bold">
+            Scroll
+          </div>
+          <ChevronDown className="text-primary h-4 w-4" />
+        </div>
+      </div>
+    </main>
   );
 }
