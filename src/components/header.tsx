a02243a34665d4511b9c27327cabed16fc7115a3
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2025-08-26 18:57:11 +0900

    refactor: convert to arrow function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export function Header() {
+export const Header = () => {
   return (
     <header className="fixed z-50 flex w-full items-center justify-between bg-white p-4 opacity-90 shadow-[0px_3px_16px_0px_rgba(0,143,238,0.05)] md:px-6">
       <Link href="/" className="text-xl font-bold text-blue-600">
@@ -15,4 +15,4 @@ export function Header() {
       </Link>
     </header>
   );
-}
+};
