7094200375603b62905cadf9a73a61cabe4ad1c8
takumi0706 <ganndamu0706@gmail.com>
2025-09-20 10:42:44 -0700

    feat: スタッフセクション、コアスタッフセクションの花形作成

---
+++ src/components/core-staff-section/index.tsx
@@ -0,0 +1,36 @@
+const coreStaffList = [
+  {
+    name: "tsuyuni",
+    image: "/coreStaff/tsuyuni.jpg",
+    href: "https://x.com/tsuy_uni",
+  }
+];
+
+export const CoreStaffSection = () => {
+  return (
+    <section className="pb-10 md:pb-20 bg-blue-light-100">
+      <h2 className="text-24 lg:text-28 leading-normal text-center font-bold pb-8 md:pb-10">
+        コアスタッフ
+      </h2>
+      <ul className="grid grid-cols-[repeat(2,120px)] md:grid-cols-[repeat(2,144px)] lg:grid-cols-[repeat(4,144px)] justify-center gap-x-14 gap-y-8 px-10">
+        {coreStaffList.map(({ name, image, href }) => (
+          <li key={name}>
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center gap-2 aspect-square"
+            >
+              <img
+                src={image}
+                alt={`${name} アイコン`}
+                className="w-full h-full object-cover rounded-full overflow-hidden"
+              />
+              <p className="text-16 leading-7">{name}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
