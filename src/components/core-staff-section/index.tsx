599f47e7a0a4d4d0eba8a2ebc07b4ce2be7ffdc3
takumi0706 <ganndamu0706@gmail.com>
2025-09-20 11:04:03 -0700

    fix: tailwindCSSのベースクラスに適応

--- src/components/core-staff-section/index.tsx
+++ src/components/core-staff-section/index.tsx
@@ -9,7 +9,7 @@ const coreStaffList = [
 export const CoreStaffSection = () => {
   return (
     <section className="bg-blue-light-100 pb-10 md:pb-20">
-      <h2 className="text-24 lg:text-28 pb-8 text-center leading-normal font-bold md:pb-10">
+      <h2 className="text-2xl lg:text-3xl pb-8 text-center leading-normal font-bold md:pb-10">
         コアスタッフ
       </h2>
       <ul className="grid grid-cols-[repeat(2,120px)] justify-center gap-x-14 gap-y-8 px-10 md:grid-cols-[repeat(2,144px)] lg:grid-cols-[repeat(4,144px)]">
@@ -26,7 +26,7 @@ export const CoreStaffSection = () => {
                 alt={`${name} アイコン`}
                 className="h-full w-full overflow-hidden rounded-full object-cover"
               />
-              <p className="text-16 leading-7">{name}</p>
+              <p className="text-base leading-7">{name}</p>
             </a>
           </li>
         ))}
