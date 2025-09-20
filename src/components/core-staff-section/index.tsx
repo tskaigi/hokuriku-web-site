83d347d947a4cc5c362122615602db74480a3c6b
takumi0706 <ganndamu0706@gmail.com>
2025-09-20 10:45:01 -0700

    fix: format-fix

--- src/components/core-staff-section/index.tsx
+++ src/components/core-staff-section/index.tsx
@@ -3,28 +3,28 @@ const coreStaffList = [
     name: "tsuyuni",
     image: "/coreStaff/tsuyuni.jpg",
     href: "https://x.com/tsuy_uni",
-  }
+  },
 ];
 
 export const CoreStaffSection = () => {
   return (
-    <section className="pb-10 md:pb-20 bg-blue-light-100">
-      <h2 className="text-24 lg:text-28 leading-normal text-center font-bold pb-8 md:pb-10">
+    <section className="bg-blue-light-100 pb-10 md:pb-20">
+      <h2 className="text-24 lg:text-28 pb-8 text-center leading-normal font-bold md:pb-10">
         コアスタッフ
       </h2>
-      <ul className="grid grid-cols-[repeat(2,120px)] md:grid-cols-[repeat(2,144px)] lg:grid-cols-[repeat(4,144px)] justify-center gap-x-14 gap-y-8 px-10">
+      <ul className="grid grid-cols-[repeat(2,120px)] justify-center gap-x-14 gap-y-8 px-10 md:grid-cols-[repeat(2,144px)] lg:grid-cols-[repeat(4,144px)]">
         {coreStaffList.map(({ name, image, href }) => (
           <li key={name}>
             <a
               href={href}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-col items-center gap-2 aspect-square"
+              className="flex aspect-square flex-col items-center gap-2"
             >
               <img
                 src={image}
                 alt={`${name} アイコン`}
-                className="w-full h-full object-cover rounded-full overflow-hidden"
+                className="h-full w-full overflow-hidden rounded-full object-cover"
               />
               <p className="text-16 leading-7">{name}</p>
             </a>
@@ -33,4 +33,4 @@ export const CoreStaffSection = () => {
       </ul>
     </section>
   );
-}
+};
