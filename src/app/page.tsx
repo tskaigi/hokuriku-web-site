0eac8702ca025a6b3ec302b25b410580cb02219e
wata <ishiwatahirotaka1012@gmail.com>
2025-09-30 18:30:14 +0900

    fix: correct filename from spondor-board.tsx to sponsor-board.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { CfpRecruitment } from "@/components/cfp-recruitment";
 import Hero from "@/components/hero";
-import { SponsorBoardSection } from "@/components/spondor-board";
+import { SponsorBoardSection } from "@/components/sponsor-board";
 import TicketPurchaseSection from "@/components/ticket-purchase";
 import { TravelCouponSection } from "@/components/travel-coupon";
 
