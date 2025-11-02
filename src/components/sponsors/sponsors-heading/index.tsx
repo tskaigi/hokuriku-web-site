5714f1b079263fb7cfc3b2bc48d040b7d9c17647
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2025-11-02 09:47:27 +0900

    feat: add sponsorClass id to h2

--- src/components/sponsors/sponsors-heading/index.tsx
+++ src/components/sponsors/sponsors-heading/index.tsx
@@ -1,25 +1,26 @@
-import { sponsorList } from "@/constants/sponsors";
+import { type SponsorClass } from "@/constants/sponsors";
+import { cn } from "@/lib/utils";
 
-const borderColor: { [key: string]: string } = {
+const borderColor = {
   platinum: "border-sponsor-platinum",
   gold: "border-sponsor-gold",
   silver: "border-sponsor-silver",
-} satisfies { [key in keyof typeof sponsorList]: string };
+} satisfies { [Key in SponsorClass]: string };
 
-const textColor: { [key: string]: string } = {
+const textColor = {
   platinum: "text-sponsor-platinum",
   gold: "text-sponsor-gold",
   silver: "text-sponsor-silver",
-} satisfies { [key in keyof typeof sponsorList]: string };
+} satisfies { [Key in SponsorClass]: string };
 
-const SponsorHeading = ({ variant }: { variant: string }) => {
+const SponsorHeading = ({ rank }: { rank: SponsorClass }) => {
   return (
     <div className="flex items-center justify-center gap-3">
-      <hr className={`flex-1 border-t ${borderColor[variant]}`} />
-      <h2 className={`text-2xl font-bold ${textColor[variant]} md:text-3xl lg:text-3xl`}>
-        {`${variant[0].toUpperCase()}${variant.slice(1)}`} Sponsors
+      <hr className={`flex-1 border-t ${borderColor[rank]}`} />
+      <h2 id={rank} className={cn("text-2xl font-bold md:text-3xl lg:text-3xl", textColor[rank])}>
+        {`${rank[0].toUpperCase()}${rank.slice(1)}`} Sponsors
       </h2>
-      <hr className={`flex-1 border-t ${borderColor[variant]}`} />
+      <hr className={`flex-1 border-t ${borderColor[rank]}`} />
     </div>
   );
 };
