c31143d23e570adf7f3db963e12357147ddb895b
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2025-11-11 22:53:05 +0900

    feat: add track to talks.json

--- scripts/generate-talks-json.ts
+++ scripts/generate-talks-json.ts
@@ -1,10 +1,14 @@
 import { writeFileSync } from "fs";
 import { join } from "path";
-import { talkList } from "../src/constants/timetableEventData";
+import { talkList, TRACK } from "../src/constants/timetableEventData";
 
 const publicDir = join(process.cwd(), "public");
 
 // talks.json を生成
-writeFileSync(join(publicDir, "talks.json"), JSON.stringify(talkList, null, 2), "utf-8");
+writeFileSync(
+  join(publicDir, "talks.json"),
+  JSON.stringify({ track: TRACK, talks: talkList }, null, 2),
+  "utf-8",
+);
 
 console.log("✅ talks.json generated at public/talks.json");
